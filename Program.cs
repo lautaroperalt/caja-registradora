8b0c8b63e7ae6d21f9b3573c84b59d8c973bcb28
Lautaro Peralta <pperalta.lautaro@gmail.com>
2026-08-10 12:32:19 -0300

    etapa 3: carga multiple con menu

--- Program.cs
+++ Program.cs
@@ -6,11 +6,42 @@ string nombreCajero = Console.ReadLine();
 Console.WriteLine();
 Console.WriteLine($"Bienvenido, {nombreCajero}. Caja abierta");
 
-Console.WriteLine("Nombre del producto que desea agregar? ");
-string nombreProducto = Console.ReadLine();
-Console.Write("Ingrese su precio: ");
-int precioProducto = Console.ReadLine();
+//las variables para ir acumulando ( se tienen que poner afuera del bucle)
+decimal cantidadTotalVentas = 0;
+int cantidadProductos = 0;
+string opcion;
 
-decimal precioActualizado = decimal.Parse(precioProducto());
+do 
+{
+    Console.WriteLine("Que desea hacer?");
+    Console.WriteLine("1. Cargar un producto"); Console.WriteLine("2. Cerrar la venta"); Console.WriteLine("Seleccione una opcion");
+    opcion = Console.ReadLine();
 
-Console.Write($"El nombre es: {nombreProducto}, y su respectivo precio: {precioProducto}");
+        switch (opcion)
+        {
+            case "1":
+                Console.WriteLine("Nombre del producto que desea agregar? ");
+                string nombreProducto = Console.ReadLine();
+
+                Console.Write("Ingrese su precio: ");
+                decimal precioProducto = decimal.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Producto '{nombreProducto}' agregado exitosamente, y su respectivo precio de {precioProducto} pesos");
+            Console.WriteLine();
+                cantidadProductos ++;
+                cantidadTotalVentas += precioProducto;
+                
+                break;
+
+            case "2":
+                Console.WriteLine("Cerrando caja... Resumen ventas: ");
+            Console.WriteLine($"Cantidad de productos agregados {cantidadProductos}");
+            Console.WriteLine($"Total ventas {cantidadTotalVentas}");
+            break;
+
+            default:
+                Console.WriteLine("Opcion invalida. Intentar nuevamente");
+                break;
+        }
+    }
+while (opcion != "2");
