92f2b24bbb11c6090f7b162a61b00d21af66615a
Lautaro Peralta <pperalta.lautaro@gmail.com>
2026-08-10 10:05:28 -0300

    etapa 1: bienvenida y datos del cajero

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Kiosco de NOB");
+
+Console.Write("Ingrese el nombre del cajero: ");
+string nombreCajero = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine($"Bienvenido, {nombreCajero}. Caja abierta");
