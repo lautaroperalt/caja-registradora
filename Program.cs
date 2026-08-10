f2b95d5b74accafec962272d5a1924ce407bfde7
Lautaro Peralta <pperalta.lautaro@gmail.com>
2026-08-10 10:14:57 -0300

    etapa 2: carga de un producto

--- Program.cs
+++ Program.cs
@@ -5,3 +5,12 @@ string nombreCajero = Console.ReadLine();
 
 Console.WriteLine();
 Console.WriteLine($"Bienvenido, {nombreCajero}. Caja abierta");
+
+Console.WriteLine("Nombre del producto que desea agregar? ");
+string nombreProducto = Console.ReadLine();
+Console.Write("Ingrese su precio: ");
+int precioProducto = Console.ReadLine();
+
+decimal precioActualizado = decimal.Parse(precioProducto());
+
+Console.Write($"El nombre es: {nombreProducto}, y su respectivo precio: {precioProducto}");
